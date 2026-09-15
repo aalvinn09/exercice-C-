1c05b0102d8dfc080087285d151183f4199ac645
aalvinn09 <alvinaddo.fk@gmail.com>
2026-09-15 15:43:29 +0200

    Exercice 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿decimal solde = 1000m;
+decimal montant = 150m;
+
+if (montant > 0)
+{
+    solde += montant;
+}
+else if (montant < 0)
+{
+    decimal debit = -montant;
+
+    if (debit <= solde)
+    {
+        solde -= debit;
+    }
+    else
+    {
+        Console.WriteLine("Solde insuffisant");
+    }
+}
+
+Console.WriteLine($"Solde : {solde}");
