4c475d8414a98c06fe3286fc9ccb41d3e1da5f8f
aalvinn09 <alvinaddo.fk@gmail.com>
2026-09-16 23:41:25 +0200

    Exercice 5

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
 ﻿
-
 Compte compte = new Compte();
 
-compte.Numero = "FR001";
 compte.Titulaire = "Alice";
-compte.Solde = 1000m;
 
-Console.WriteLine($"Solde de départ : {compte.Solde}");
+compte.Crediter(1000m);
+
+Console.WriteLine($"Titulaire : {compte.Titulaire}");
+Console.WriteLine($"Solde : {compte.Solde}");
 
 compte.Crediter(200m);
-Console.WriteLine($"Apres credit : {compte.Solde}");
+Console.WriteLine($"Après crédit : {compte.Solde}");
 
 compte.Debiter(50m);
-Console.WriteLine($"Apres debit : {compte.Solde}");
+Console.WriteLine($"Après débit : {compte.Solde}");
+
 
 class Compte
 {
-    public string Numero {get; set;}
-    public string Titulaire {get; set}
-    public decimal Solde {get; set}
+    public string Numero { get; private set; }
+    public string Titulaire { get; set; }
+    public decimal Solde { get; private set; }
 
-    public void Crediter (decimal montant)
+    public void Crediter(decimal montant)
     {
         if (montant > 0)
         {
@@ -28,20 +29,18 @@ class Compte
         }
     }
 
-
-public void Debiter(decimal montant)
-{     
-    if (montant > 0 && montant <= Solde)
+    public void Debiter(decimal montant)
     {
-    Solde = Montant;
+        if (montant > 0 && montant <= Solde)
+        {
+            Solde -= montant;
+        }
+        else
+        {
+            Console.WriteLine("Débit impossible");
+        }
     }
-else
-{
-    Console.WriteLine("Debit impossible");
 }
-}
-}
-
 
 
  
