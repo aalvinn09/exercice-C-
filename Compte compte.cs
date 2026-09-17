02cb65d1fe123bbe34931f6b0a82707f25e5b900
aalvinn09 <alvinaddo.fk@gmail.com>
2026-09-17 08:53:45 +0200

    Exercice 5

---
+++ Compte compte.cs
@@ -0,0 +1,28 @@
+
+
+class Compte
+{
+    public string Numero { get; set; }
+    public string Titulaire { get; set; }
+    public decimal Solde { get; set; }
+
+    public void Crediter(decimal montant)
+    {
+        if (montant > 0)
+        {
+            Solde += montant;
+        }
+    }
+
+    public void Debiter(decimal montant)
+    {
+        if (montant > 0 && montant <= Solde)
+        {
+            Solde -= montant;
+        }
+        else
+        {
+            Console.WriteLine("Débit impossible.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,32 +15,4 @@ compte.Debiter(50m);
 Console.WriteLine($"Après débit : {compte.Solde}");
 
 
-class Compte
-{
-    public string Numero { get; private set; }
-    public string Titulaire { get; set; }
-    public decimal Solde { get; private set; }
-
-    public void Crediter(decimal montant)
-    {
-        if (montant > 0)
-        {
-            Solde += montant;
-        }
-    }
-
-    public void Debiter(decimal montant)
-    {
-        if (montant > 0 && montant <= Solde)
-        {
-            Solde -= montant;
-        }
-        else
-        {
-            Console.WriteLine("Débit impossible");
-        }
-    }
-}
-
-
  
