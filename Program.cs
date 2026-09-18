60a235ffc1493afd7ef5c7ad6ad57578a8034a6a
aalvinn09 <alvinaddo.fk@gmail.com>
2026-09-18 11:29:42 +0200

    Exercice 8

--- Program.cs
+++ Program.cs
@@ -1,18 +1,7 @@
 ﻿
-Compte compte = new Compte("FR001", "Alice");
-Compte compte2 = new Compte("FR002", "Bob", 500m);
+var compte1 = new Compte("FR001", "Alice", 1000m);
+var compte2 = compte1;
 
-Console.WriteLine("Compte 1");
-Console.WriteLine($"Numéro : {compte1.Numero}");
-Console.WriteLine($"Titulaire : {compte1.Titulaire}");
-Console.WriteLine($"Solde  : {compte1.Solde}");
+compte2.Crediter(500m);
 
-Console.WriteLine();
-
-
-Console.WriteLine("Compte 2");
-Console.WriteLine($"Numéro : {compte2.Numero}");
-Console.WriteLine($"Titulaire : {compte2.Titulaire}");
-Console.WriteLine($"Solde : {compte2.Solde}");
-
- 
+Console.WriteLine(compte1.Solde);
